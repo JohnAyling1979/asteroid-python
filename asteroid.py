6b2a7c5d3d80123d62b0878b60262e89177064fc
John Ayling <johnayling@yahoo.com>
2025-06-03 06:22:04 -0500

    Add asteroid splitting.

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
   def __init__(self, x, y, radius):
@@ -10,3 +12,22 @@ class Asteroid(CircleShape):
 
   def update(self, dt):
     self.position += self.velocity * dt
+
+  def split(self):
+    self.kill()
+
+    if (self.radius < ASTEROID_MIN_RADIUS):
+      return
+
+    angle = random.uniform(20, 50)
+
+    velocity1 = self.velocity.rotate(angle) * 1.2
+    velocity2 = self.velocity.rotate(-angle) * 1.2
+    radius = self.radius - ASTEROID_MIN_RADIUS
+
+    asteroid1 = Asteroid(self.position.x, self.position.y, radius)
+    asteroid2 = Asteroid(self.position.x, self.position.y, radius)
+
+    asteroid1.velocity = velocity1
+    asteroid2.velocity = velocity2
+
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, SHOT_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, SHOT_RADIUS, PLAYER_SHOOT_COOLDOWN
 from circleshape import CircleShape
 from shot import Shot
 
@@ -7,6 +7,7 @@ class Player(CircleShape):
   def __init__(self, x, y):
     super().__init__(x, y, PLAYER_RADIUS)
     self.rotation = 0
+    self.cooldown = 0
 
   def triangle(self):
     forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -26,26 +27,28 @@ class Player(CircleShape):
   def shoot(self):
     shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
     shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+    self.cooldown = PLAYER_SHOOT_COOLDOWN
 
   def update(self, dt):
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_a]:
+    if (keys[pygame.K_a]):
       self.rotate(-dt)
 
-    if keys[pygame.K_d]:
+    if (keys[pygame.K_d]):
       self.rotate(dt)
 
-    if keys[pygame.K_w]:
+    if (keys[pygame.K_w]):
       self.move(dt)
 
-    if keys[pygame.K_s]:
+    if (keys[pygame.K_s]):
       self.move(-dt)
 
-    if keys[pygame.K_SPACE]:
+    if (keys[pygame.K_SPACE] and self.cooldown <= 0):
       self.shoot()
 
-
+    if (self.cooldown > 0):
+      self.cooldown -= dt
 
   def draw(self, screen):
     pygame.draw.polygon(screen, (255, 255, 255), self.triangle())
